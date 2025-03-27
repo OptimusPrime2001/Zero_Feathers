07508e5632234e029248c2ab1573ba946000a014
letrunks <trungbts3@gmail.com>
2025-03-27 23:11:58 +0700

    feat: update app service response and add HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/dainel_le-key.pem'),
+  cert: fs.readFileSync('./secrets/dainel_le.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log('PORT: ', process.env.PORT);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+  await app.init();
+  await app.listen(process.env.PORT || 3000);
+  // const configService = app.get(ConfigService);
+  // await app.listen(configService.get('PORT') as number);
 }
 bootstrap();
